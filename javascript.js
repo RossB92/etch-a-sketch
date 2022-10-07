1847f5b858d174f253f07d838ecccbcde8a6e963
Ross <rosswbillington@gmail.com>
2022-10-07 00:36:22 -0400

    Create slider to determine number of boxes

--- javascript.js
+++ javascript.js
@@ -11,44 +11,59 @@ let container = document.getElementById("container");
 let box = document.getElementById("box");
 let slider= document.getElementById('myRange');
 let output = document.getElementById('output');
-output.innerHTML = slider.value;
-let slideValue = 32;
+
+var values = [4,9,16,25,36,49,64];
+// slider.oninput = function(){
+//     output.innerHTML = values[this.value];
+//     let secVal = +output.innerHTML;
+//     console.log(typeof(secVal))
+//     return secVal
+// };
+
+// output.innerHTML = slider.value;
 slider.oninput = function() {
-    output.innerHTML = this.value;
-    let slideValue = +output.innerHTML;
-    console.log(slideValue);
-    return slideValue;
+    output.innerHTML = values[this.value];
 }
+slider.oninput(25);
 function createGrid() {
-    let numOfCols = slider.value/2 // +prompt("How many Columns?");
-    let numOfRows = slider.value/2//+prompt("How many Rows?");  
-    //Creates number of rows
-    for (let r = 1; r <= numOfRows; r++) {
-        let row = document.createElement("div");
-        row.className = "row";
-        // Creates number of columns
-        for (let c = 1; c <= numOfCols; c++) {
-            let box = document.createElement("div");
-            box.className = "box";
-            box.addEventListener('mouseenter', (e) => {
-                // Allows color fade in/out 
-                //box.className = "box hovering";
-                box.style.backgroundColor= randomColor();
-                box.addEventListener('mouseout', (e) => {
-                    // Allow color fade in/out 
-                    //box.className = "box notHovering";
+    if(document.getElementById('box')) {
+        let deleted = document.getElementById('box');
+        deleted.remove();
+    } else {
+        let slideValue = +output.innerHTML;
+
+        console.log(slideValue)
+
+        let numOfCols = Math.sqrt(slideValue); // +prompt("How many Columns?");
+        let numOfRows = Math.sqrt(slideValue);//+prompt("How many Rows?");  
+        //Creates number of rows
+        for (let r = 1; r <= numOfRows; r++) {
+            let row = document.createElement("div");
+            row.className = "row";
+            // Creates number of columns
+            for (let c = 1; c <= numOfCols; c++) {
+                let box = document.createElement("div");
+                box.className = "box";
+                box.addEventListener('mouseenter', (e) => {
+                    // Allows color fade in/out 
+                    //box.className = "box hovering";
+                    box.style.backgroundColor= randomColor();
+                    box.addEventListener('mouseout', (e) => {
+                        // Allow color fade in/out 
+                        //box.className = "box notHovering";
+                    })
                 })
-            })
-            
-            row.appendChild(box);
-            // Adds color on hover
-            
+                
+                row.appendChild(box);
+                // Adds color on hover
+                
 
 
+                
+            }
             
+            container.appendChild(row);
         }
-        
-        container.appendChild(row);
     }
 }
 
