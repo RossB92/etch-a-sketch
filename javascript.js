7f4b466ef258d1869c5f18ecb80f046144d126bb
Ross <rosswbillington@gmail.com>
2022-10-07 11:21:24 -0400

    Update output.html to better wording; update button wording

--- javascript.js
+++ javascript.js
@@ -1,69 +1,68 @@
-let grid = document.getElementById('grid');
+let grid = document.getElementById("grid");
 let container = document.getElementById("container");
-
-let box = document.getElementById("box");
-let slider= document.getElementById('myRange');
-let output = document.getElementById('output');
-
-let deletedBox = document.getElementsByClassName('box');
+let slider = document.getElementById("myRange");
+let output = document.getElementById("output");
 
 let gridMade;
 
-var values = [4,9,16,25,36,49,64];
+var values = [ 4, 9, 16, 25, 36, 49, 64, 81, 100 ];
 slider.oninput = function() {
-    output.innerHTML = values[this.value];
-}
+	output.innerHTML = "Number of " + values[this.value] + " boxes";
+	output.value = values[this.value];
+};
 slider.oninput(25);
 function createGrid() {
-
-    if(gridMade===1) {
-        gridMade = 0;
-        clearGrid();
-        grid.appendChild(container);
-        createBoxes();
-    } else {
-        gridMade = 1;
-        let slideValue = +output.innerHTML;
-        let numOfCols = Math.sqrt(slideValue); // +prompt("How many Columns?");
-        let numOfRows = Math.sqrt(slideValue);//+prompt("How many Rows?");  
-        //Creates number of rows
-        for (let r = 1; r <= numOfRows; r++) {
-            let row = document.createElement("div");
-            row.className = "row";
-            // Creates number of columns
-            for (let c = 1; c <= numOfCols; c++) {
-                let box = document.createElement("div");
-                box.className = "box";
-                box.addEventListener('mouseenter', (e) => {
-                    // Allows color fade in/out 
-                    //box.className = "box hovering";
-                    box.style.backgroundColor= randomColor();
-                    box.addEventListener('mouseout', (e) => {
-                        // Allow color fade in/out 
-                        //box.className = "box notHovering";
-                    })
-                })
-                
-                row.appendChild(box);
-                // Adds color on hover
-            }
-            container.appendChild(row);
-        } return gridMade;
-    }
+	if (gridMade === 1) {
+		clearGrid();
+		createBoxes();
+	} else {
+		createBoxes();
+	}
 }
 
 function randomColor() {
-    let o = Math.round, r = Math.random, s = 255;
-    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+	let o = Math.round,
+		r = Math.random,
+		s = 255;
+	return "rgba(" + o(r() * s) + "," + o(r() * s) + "," + o(r() * s) + "," + r().toFixed(1) + ")";
 }
 
 function clearGrid() {
-    container.remove();
-    container = document.createElement('div')
-    container.className = 'container'
-    container.id = 'container';
+	gridMade = 0;
+	container.remove();
+	container = document.createElement("div");
+	container.className = "container";
+	container.id = "container";
+	grid.appendChild(container);
 }
 
 function createBoxes() {
-    
+	gridMade = 1;
+	let slideValue = +output.value;
+	let numOfCols = Math.sqrt(slideValue); // +prompt("How many Columns?");
+	let numOfRows = Math.sqrt(slideValue); //+prompt("How many Rows?");
+	//Creates number of rows
+	for (let r = 1; r <= numOfRows; r++) {
+		let row = document.createElement("div");
+		row.className = "row";
+		// Creates number of columns
+		for (let c = 1; c <= numOfCols; c++) {
+			let box = document.createElement("div");
+			box.className = "box";
+			box.addEventListener("mouseenter", (e) => {
+				// Allows color fade in/out
+				//box.className = "box hovering";
+				box.style.backgroundColor = randomColor();
+				box.addEventListener("mouseout", (e) => {
+					// Allow color fade in/out
+					//box.className = "box notHovering";
+				});
+			});
+
+			row.appendChild(box);
+			// Adds color on hover
+		}
+		container.appendChild(row);
+	}
+	return gridMade;
 }
